0d08ae54c3aac29fbb581bc7401379960838bd28
Christyan Santos <christyanrosa@hotmail.com>
2021-06-03 18:30:31 -0300

    feat: #57 create enemy spawn manager (#63)

---
+++ Assets/Scripts/Behaviours/Managers/EnemyManager.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Linq;
+using Behaviours.Player;
+using Enums;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Behaviours.Managers
+{
+    public class EnemyManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject enemy;
+        [SerializeField] private Transform[] spawnPoints;
+        [SerializeField] [Range(1, 100)] private float spawnTime = 3f;
+
+        private PlayerHealth _playerHealth;
+
+
+        private void Start()
+        {
+            _playerHealth = GameObject.FindGameObjectWithTag(Tag.Player.ToString()).GetComponent<PlayerHealth>();
+
+            Validate();
+
+            InvokeRepeating(nameof(Spawn), spawnTime, spawnTime);
+        }
+
+        private void Validate()
+        {
+            if (_playerHealth == null)
+                throw new ArgumentException("No player health found");
+
+
+            if (enemy == null)
+                throw new ArgumentException("No enemy found");
+
+            if (!spawnPoints.Any())
+                throw new ArgumentException("No spawn point setup");
+        }
+
+
+        private void Spawn()
+        {
+            if (_playerHealth.IsAlive)
+            {
+                var spawnPointIndex = Random.Range(0, spawnPoints.Length);
+                Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            }
+        }
+    }
+}
