df212d3a98bb75a4e89e9969c6a5ad41b6600e22
Christyan Santos Rosa <christyanrosa@hotmail.com>
2021-06-02 21:11:07 -0300

    feat: #47 add camera setup to follow the player

---
+++ Assets/Scripts/Enums/Axis.cs
@@ -0,0 +1,8 @@
+namespace Enums
+{
+    public enum Axis
+    {
+        Horizontal = 0,
+        Vertical = 1
+    }
+}
---
+++ Assets/Scripts/Enums/Tag.cs
@@ -0,0 +1,7 @@
+namespace Enums
+{
+    public enum Tag
+    {
+        Player = 1
+    }
+}
