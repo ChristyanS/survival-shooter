be4a979fadf3e87d425b02c047786437c66a75de
Christyan Santos Rosa <christyanrosa@hotmail.com>
2021-06-03 19:08:33 -0300

    feat: #58 add game over when character dies

---
+++ Assets/Scripts/Behaviours/Managers/GameOverManager.cs
@@ -0,0 +1,41 @@
+using System;
+using Behaviours.Player;
+using Enums;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Behaviours.Managers
+{
+    public class GameOverManager : MonoBehaviour
+    {
+        private static readonly int GameOver = Animator.StringToHash("GameOver");
+        private Animator _animator;
+        private PlayerHealth _playerHealth;
+
+        private void Start()
+        {
+            _playerHealth = GameObject.FindGameObjectWithTag(Tag.Player.ToString()).GetComponent<PlayerHealth>();
+            _animator = GetComponent<Animator>();
+            Validate();
+        }
+
+        private void Update()
+        {
+            if (!_playerHealth.IsAlive)
+            {
+                _animator.SetTrigger(GameOver);
+                if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.R) ||
+                    Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter))
+                    SceneManager.LoadScene("level");
+            }
+        }
+
+        private void Validate()
+        {
+            if (_playerHealth == null)
+                throw new ArgumentException("No player health found");
+            if (_animator == null)
+                throw new ArgumentException("No animator found");
+        }
+    }
+}
