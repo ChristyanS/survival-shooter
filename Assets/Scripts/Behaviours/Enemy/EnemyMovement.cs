effb594a72a8a3dae27b4671e4ef06b83fd9fd42
Christyan Santos Rosa <christyanrosa@hotmail.com>
2021-06-06 16:39:41 -0300

    fix: #66 disable enemies movement when main character has died and show game over message

--- Assets/Scripts/Behaviours/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Behaviours/Enemy/EnemyMovement.cs
@@ -1,4 +1,5 @@
 using System;
+using Behaviours.Player;
 using Enums;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,20 +8,30 @@ namespace Behaviours.Enemy
 {
     public class EnemyMovement : MonoBehaviour
     {
+        private EnemyHealth _enemyHealth;
         private NavMeshAgent _navMeshAgent;
+        private PlayerHealth _playerHealth;
         private Transform _playerTransform;
 
         public void Start()
         {
             _playerTransform = GameObject.FindGameObjectWithTag(Tag.Player.ToString()).transform;
+            _playerHealth = _playerTransform.GetComponent<PlayerHealth>();
             _navMeshAgent = GetComponent<NavMeshAgent>();
+            _enemyHealth = GetComponent<EnemyHealth>();
+
             Validate();
         }
 
         private void Update()
         {
-            if (_navMeshAgent.isOnNavMesh)
-                _navMeshAgent.SetDestination(_playerTransform.position);
+            if (_enemyHealth.IsAlive && _playerHealth.IsAlive)
+            {
+                if (_navMeshAgent.isOnNavMesh)
+                    _navMeshAgent.SetDestination(_playerTransform.position);
+            }
+            else
+                _navMeshAgent.enabled = false;
         }
 
         private void Validate()
@@ -29,6 +40,10 @@ namespace Behaviours.Enemy
                 throw new ArgumentException("No navmesh agent found");
             if (!_navMeshAgent.isOnNavMesh)
                 throw new ArgumentException("No navmesh configuration");
+            if (_playerHealth == null)
+                throw new ArgumentException("No player health found");
+            if (_enemyHealth == null)
+                throw new ArgumentException("No enemy health found");
         }
     }
 }
