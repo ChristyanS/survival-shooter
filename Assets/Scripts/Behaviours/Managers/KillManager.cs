f1d6fb46fe81d09cb8d74408ffb2e6c834889f14
Christyan Santos Rosa <christyanrosa@hotmail.com>
2021-06-11 21:39:56 -0300

    feat: #77 add player kills

--- Assets/Scripts/Behaviours/Managers/KillManager.cs
+++ Assets/Scripts/Behaviours/Managers/KillManager.cs
@@ -4,21 +4,21 @@ using UnityEngine.UI;
 
 namespace Behaviours.Managers
 {
-    public class ScoreManager : MonoBehaviour
+    public class KillManager : MonoBehaviour
     {
-        public static int Score;
+        public static int Kills;
         private Text _text;
 
         private void Start()
         {
             _text = GetComponent<Text>();
-            Score = 0;
+            Kills = 0;
             Validate();
         }
 
         private void Update()
         {
-            _text.text = "Score: " + Score;
+            _text.text = Kills.ToString();
         }
 
         private void Validate()
